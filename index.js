f38d32d23306ffa770cb7ffb2c1c37d21748ba07
Lilla <66587693+epsseniyer@users.noreply.github.com>
2022-08-15 13:40:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TaskInput = document.querySelector(".task-input input"),
+const taskInput = document.querySelector(".task-input input"),
     filters = document.querySelectorAll(".filters span"),
     clearAll = document.querySelector(".clear-btn"),
     taskBox = document.querySelector(".task-box");
